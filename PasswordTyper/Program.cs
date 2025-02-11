7a9472028ca4320a7a6e027cd95b2eea5c070394
Youri <yourish@live.nl>
2025-02-11 19:44:51 +0100

    Velopack attempt

--- PasswordTyper/Program.cs
+++ PasswordTyper/Program.cs
@@ -1,3 +1,6 @@
+using Velopack;
+using Velopack.Sources;
+
 namespace PasswordTyper
 {
     internal static class Program
@@ -8,16 +11,34 @@ namespace PasswordTyper
         [STAThread]
         static void Main()
         {
+            VelopackApp.Build().Run();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
 
+            UpdateApp().Wait();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TrayApp());
+        }
 
+        private static async Task UpdateApp()
+        {
+            var mgr = new UpdateManager(new GithubSource("https://github.com/yschuurmans/PasswordTyper", null, false));
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
         }
     }
 }
