84fe118ab60150fe0f17181819137e3729915151
Youri <yourish@live.nl>
2025-03-31 12:23:22 +0200

    Added time syncing to the Totp instead of adding a flat 5 seconds

--- PasswordTyper/Helpers/TotpHelper.cs
+++ PasswordTyper/Helpers/TotpHelper.cs
@@ -1,15 +1,24 @@
-﻿using OtpNet;
+﻿using GuerrillaNtp;
+using OtpNet;
 
 namespace PasswordTyper.Helpers
 {
     public static class TotpHelper
     {
+        private static NtpClock? _clock;
         public static string GenerateTotp(string secret)
         {
             var secretBytes = Base32Encoding.ToBytes(secret);
             var totp = new Totp(secretBytes);
+            if (_clock == null)
+            {
+                NtpClient client = NtpClient.Default;
+                _clock = client.Query();
+            }
 
-            return totp.ComputeTotp(DateTime.UtcNow.AddSeconds(5));
+            var utcTime = _clock.UtcNow.DateTime;
+
+            return totp.ComputeTotp(utcTime);
         }
     }
 }
